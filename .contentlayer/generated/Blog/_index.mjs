bb65f395441813380a158d2ee039264bdf647027
서후람/선임연구원/ID시스템앱Project <hooram.seo@lge.com>
2024-11-28 16:41:25 +0900

    Update the About page

--- .contentlayer/generated/Blog/_index.mjs
+++ .contentlayer/generated/Blog/_index.mjs
@@ -3,7 +3,7 @@
 import blog__codeSampleMdx from './blog__code-sample.mdx.json' assert { type: 'json' }
 import blog__newFeaturesInV1Mdx from './blog__new-features-in-v1.mdx.json' assert { type: 'json' }
 import blog__picturesOfCanadaMdx from './blog__pictures-of-canada.mdx.json' assert { type: 'json' }
-import blog__theTimeMachineMdx from './blog__the-time-machine.mdx.json' assert { type: 'json' }
 import blog__testMdx from './blog__Test.mdx.json' assert { type: 'json' }
+import blog__theTimeMachineMdx from './blog__the-time-machine.mdx.json' assert { type: 'json' }
 
-export const allBlogs = [blog__codeSampleMdx, blog__newFeaturesInV1Mdx, blog__picturesOfCanadaMdx, blog__theTimeMachineMdx, blog__testMdx]
+export const allBlogs = [blog__codeSampleMdx, blog__newFeaturesInV1Mdx, blog__picturesOfCanadaMdx, blog__testMdx, blog__theTimeMachineMdx]
