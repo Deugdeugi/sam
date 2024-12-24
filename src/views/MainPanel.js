b361bfb358c4e8e92996abb88da714a6c4357068
서후람/선임연구원/ID시스템앱Project <hooram.seo@lge.com>
2024-12-24 17:13:10 +0900

    Create Gallery view

--- src/views/MainPanel.js
+++ src/views/MainPanel.js
@@ -9,106 +9,143 @@ import Item from '@enact/sandstone/Item';
 import Skinnable from '@enact/sandstone/Skinnable';
 import BodyText from '@enact/sandstone/BodyText';
 import Scroller from '@enact/sandstone/Scroller';
+import css from './MainPanel.module.less';
+import {VirtualList, VirtualGridList} from '@enact/sandstone/VirtualList';
+import ImageItem from '@enact/sandstone/ImageItem';
+import ri from '@enact/ui/resolution';
+import IconItem from '@enact/sandstone/IconItem';
+import KeyGuide from '@enact/sandstone/KeyGuide';
 
 const MainPanel = (props) => {
 	const [tabIndex, setTabIndex] = useState(0);
 
+	const renderItem = ({index, ...rest}) => {
+		return (
+			<ImageItem
+				index={index}
+				label="ImageItem label"
+				orientation="vertical"
+				src={'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 300 300\' width=\'300\' height=\'300\'%3E%3Crect width=\'300\' height=\'300\' fill=\'%237ed31d\'%3E%3C/rect%3E%3Ctext x=\'50%25\' y=\'50%25\' dominant-baseline=\'middle\' text-anchor=\'middle\' font-size=\'36px\' fill=\'%23ffffff\'%3E300 X 300%3C/text%3E%3C/svg%3E'}
+				onFocus={
+					() => {
+						setTabIndex(index % 2);
+					}
+				}
+				// style={{
+				// 	width: '400px',
+				// 	height: '340px',
+				// 	position: 'absolute',
+				// 	padding: '10px 10px 10px 10px'
+				// }}
+				{...rest}
+				>
+				ImageItem Caption
+			</ImageItem>
+		);
+	}
+
+	const renderSubItem = ({index, ...rest}) => {
+		return (
+			( tabIndex == 0 ) ?
+			<ImageItem
+				index={index}
+				label="ImageItem label"
+				orientation="vertical"
+				src={'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 200 200\' width=\'200\' height=\'200\'%3E%3Crect width=\'200\' height=\'200\' fill=\'%237ed31d\'%3E%3C/rect%3E%3Ctext x=\'50%25\' y=\'50%25\' dominant-baseline=\'middle\' text-anchor=\'middle\' font-size=\'36px\' fill=\'%23ffffff\'%3E200 X 200%3C/text%3E%3C/svg%3E'}
+				// style={{
+				// 	width: '360px',
+				// 	height: '240px',
+				// 	position: 'absolute',
+				// 	padding: '10px 10px 10px 10px'
+				// }}
+				{...rest}
+				>
+				ImageItem Caption
+			</ImageItem>
+			: 
+			<ImageItem
+			index={index}
+				label="ImageItem label"
+				orientation="vertical"
+				src={'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 200 200\' width=\'200\' height=\'200\'%3E%3Crect width=\'100\' height=\'100\' fill=\'%237ed31d\'%3E%3C/rect%3E%3Ctext x=\'50%25\' y=\'50%25\' dominant-baseline=\'middle\' text-anchor=\'middle\' font-size=\'36px\' fill=\'%23ffffff\'%3E200 X 200%3C/text%3E%3C/svg%3E'}
+				// style={{
+				// 	width: '360px',
+				// 	height: '240px',
+				// 	position: 'absolute',
+				// 	padding: '10px 10px 10px 10px'
+				// }}
+				{...rest}
+			>
+				ImageItem Caption
+			</ImageItem>
+		);
+	}
+
 	return (
-		<Panels index={tabIndex}>
+		<Panels /*index={tabIndex}*/>
 			<Panel {...props} >
-				<Header title="Hello world!" noCloseButton />
-				<TabLayout
-					collapsed={false}
-				>
-					<Tab title="Tab One">
+				<Header title="Hello world!" noCloseButton >
+					<slotAfter>
 						<Button
-							iconPosition="before"
-							minWidth
-							onClick={() => {
-								setTabIndex(1);
+							backgroundOpacity='transparent'
+							style={{
+								minWidth: 0,
+								lineHeight: 0,
+								padding: '0 0.5rem'
 							}}
-							onTap={() => {
-								setTabIndex(1);
-							}}
-							size="large"
 						>
-							click me
+							<Icon size="small">
+								info
+							</Icon>
 						</Button>
-					</Tab>
-					<Tab title="Tab Two">
-						<Item>Goodbye</Item>
-					</Tab>
-				</TabLayout>
-			</Panel>
-			<Panel
-				onBack={() => {
-					setTabIndex(tabIndex - 1);
-				}}
-			>
-				<Header
-					title="Panel with Items"
-				>
-					<Button
-						icon="arrowlargeright"
-						iconFlip="auto"
-						onClick={() => {
-							setTabIndex(tabIndex + 1);
-						}}
-						size="small"
-						slot="slotAfter"
-					/>
+					</slotAfter>
 				</Header>
-				<BodyText>
-					Example text inside an Panel Body
-				</BodyText>
-				<Item>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</Item>
-				<Item>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</Item>
-				<Item>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</Item>
-			</Panel>
-			<Panel
-				onBack={() => {
-					setTabIndex(tabIndex - 1);
-				}}
-			>
-				<Header title="Panel with Scroller" />
-				<Scroller>
-					<div
+				{/* <IconItem
+					background="#1b1b1b"
+					bordered
+					icon="usb"
+					label='sdfsfsdf'
+					style={{
+						//width: "50%"
+					 }}
+				/> */}
+				<div
+					style={{
+						height: "50%"
+					}}
+				>
+					<VirtualGridList
+						direction="horizontal"
+						dataSize={30}
+						itemRenderer={renderItem}
+						itemSize={{minWidth: 300, minHeight: 240}}
+						horizontalScrollbar='hidden'
 						style={{
-							height: '41.75rem',
-							width: '83.375rem'
-						}}
-					>
-						<BodyText>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							<br />
-							Aenean id blandit nunc. Donec lacinia nisi vitae mi dictum, eget pulvinar nunc tincidunt. Integer vehicula tempus rutrum. Sed efficitur neque in arcu dignissim cursus.
-						</BodyText>
-						<div
-							style={{
-								marginTop: '33.375rem'
+							//height: "50%"
 							}}
-						>
-							<BodyText>
-								Mauris blandit sollicitudin mattis. Fusce commodo arcu vitae risus consectetur sollicitudin. Aliquam eget posuere orci. Cras pellentesque lobortis sapien non lacinia.
-							</BodyText>
-						</div>
-						<div
-							style={{
-								marginTop: '33.375rem'
+					/>
+				</div>
+				<div style={{height: "2%"}}></div>
+				<div
+					style={{
+						height: "40%",
+						padding: "20px",
+						backgroundColor: '#4C4C4C'
+					}}
+				>
+					{/* <div>sfsafsafsaf</div> */}
+					{/* <div style={{height: "5%"}}></div> */}
+					<VirtualGridList
+						direction="horizontal"
+						dataSize={10}
+						itemRenderer={renderSubItem}
+						itemSize={{minWidth: 150, minHeight: 150}}
+						horizontalScrollbar='hidden'
+						style={{
+							//height: "50%"
 							}}
-						>
-							<BodyText>
-								Mauris blandit sollicitudin mattis. Fusce commodo arcu vitae risus consectetur sollicitudin. Aliquam eget posuere orci. Cras pellentesque lobortis sapien non lacinia.
-							</BodyText>
-						</div>
-					</div>
-				</Scroller>
+					/>
+				</div>
 			</Panel>
 		</Panels>
 	);
